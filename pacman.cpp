8fd3bb609cdbc139b8866e6121061206d5dde8eb
Suleman Mahmood <sulemanmahmood99@gmail.com>
2019-12-08 18:23:49 +0500

    Update pacman.cpp

--- pacman.cpp
+++ pacman.cpp
@@ -20,7 +20,7 @@ const char PACMAN_CHAR = 'C';
 const char GHOST_CHAR = '9';
 const double GENERATE_FOOD_EVERY_SECOND = 0.5; // Value is in seconds
 const int SCORE_PER_FOOD = 5;
-const int GHOST_RESPAWN_TIME = 10; // Value is in seconds
+const int GHOST_RESPAWN_TIME = 5; // Value is in seconds
 
 // Prints the Array "map" on the screen
 void printMap(); 
@@ -68,7 +68,7 @@ int main()
 	int x, y; //The location of our character (pacman)
 	int waitTenSecondCounter = 0; //counter to spawn food
 	int waitGhostRespawnCounter = 0; // counter to spawn ghost
-	int Score = 0;
+	int Score = 0; // Holds the runtime score of the player
 	double totalTime = 0; //total time elapsed during the program runtime
 	int counterGhostSpawned = 0; // Holds the number of ghosts currently spawned
 	int currentSpawnedFood = 0; // Holds the number of currently spawned food
@@ -83,16 +83,21 @@ int main()
 	// Infinite Loop to run the game
 	while (1)
 	{
+		// Clears the screen
 		system("CLS");
+		// Calculates the total time elapsed
 		totalTime += 0.1;
 
+		// When Up Arrow Key is pressed
 		if (GetAsyncKeyState(VK_UP))
 		{
+			//Checks whether the new location is free space
 			if (map[y - 1][x] == ' ')
 			{
 				map[y][x] = ' ';
 				y--;
 			}
+			//Checks whether we are moving to the food location
 			else if (map[y - 1][x] == FOOD_CHAR)
 			{
 				map[y][x] = ' ';
@@ -101,9 +106,11 @@ int main()
 				currentSpawnedFood--;
 				Score += SCORE_PER_FOOD;
 			}
+			//checks whether we are moving to the ghost location
 			else if (map[y - 1][x] == GHOST_CHAR)
 				gameOver = true;
 		}
+		// When Down Arrow Key is pressed
 		else if (GetAsyncKeyState(VK_DOWN))
 		{
 			if (map[y + 1][x] == ' ')
@@ -122,6 +129,7 @@ int main()
 			else if (map[y - 1][x] == GHOST_CHAR)
 				gameOver = true;
 		}
+		// When Left Arrow Key is pressed
 		else if (GetAsyncKeyState(VK_LEFT))
 		{
 			if (map[y][x-1] == ' ')
@@ -140,6 +148,7 @@ int main()
 			else if (map[y - 1][x] == GHOST_CHAR)
 				gameOver = true;
 		}
+		// When Right Arrow Key is pressed
 		else if (GetAsyncKeyState(VK_RIGHT))
 		{
 			if (map[y][x+1] == ' ')
@@ -159,8 +168,10 @@ int main()
 				gameOver = true;
 		}
 
+		// Move the pacman to the new location or the same location
 		map[y][x] = PACMAN_CHAR;
 
+		// Generates food every Second on random location
 		waitTenSecondCounter++;
 		if (waitTenSecondCounter == GENERATE_FOOD_EVERY_SECOND * 10)
 		{
@@ -168,14 +179,16 @@ int main()
 			spawnFood(currentSpawnedFood);
 		}
 
+		//Spawns Ghost every second at a random location
 		waitGhostRespawnCounter++;
-		if (waitGhostRespawnCounter == GHOST_RESPAWN_TIME * 5)
+		if (waitGhostRespawnCounter == GHOST_RESPAWN_TIME * 10)
 		{
 			waitGhostRespawnCounter = 0;
 			ghostsLocationsArray[counterGhostSpawned] = spawnGhost();
 			counterGhostSpawned++;
 		}
 
+		// Displays runtime score
 		cout << "Score: " << Score << endl;
 	
 		//Move all Ghosts
@@ -189,7 +202,7 @@ int main()
 
 		printMap();
 
-		//Pauses the game for 100 Millisec
+		//Pauses the game for 100 Millisecond
 		Sleep(100);
 	}
 
@@ -221,9 +234,11 @@ void spawnFood(int& currentSpawnedFood)
 
 	srand(time(0));
 
+	//Only spawns a fixed number of maximum foods
 	if (currentSpawnedFood >= MAX_FOOD)
 		return;
 
+	//Finds correct location where there is empty space
 	while (!isCorrectLocation)
 	{
 		x = rand() % 25;
@@ -249,6 +264,7 @@ ghostLocation spawnGhost()
 
 	srand(time(0));
 
+	//Finds correct location where there is empty space
 	while (!isCorrectLocation)
 	{
 		x = rand() % 25;
@@ -266,9 +282,11 @@ ghostLocation spawnGhost()
 	temp.x = x;
 	temp.y = y;
 
+	//returning the location where the ghost is spawned
 	return temp;
 }
 
+// Moves the ghost in a valid direction in any random four directions (up/down/left/right)
 void moveGhost(int &x, int &y, bool& gameOver)
 {
 	bool isCorrectLocation = false;
@@ -277,8 +295,10 @@ void moveGhost(int &x, int &y, bool& gameOver)
 
 	while (!isCorrectLocation)
 	{
+		// generates a random number between 1 and 4
 		int direction = rand() % 4 + 1;
 
+		// 1 indicates up direction
 		if (direction == 1)
 		{
 			if (map[y - 1][x] != '#' && map[y - 1][x] != GHOST_CHAR)
@@ -290,6 +310,7 @@ void moveGhost(int &x, int &y, bool& gameOver)
 			}
 
 		}
+		// 2 indicates down direction
 		else if (direction == 2)
 		{
 			if (map[y + 1][x] != '#' && map[y + 1][x] != GHOST_CHAR)
@@ -300,6 +321,7 @@ void moveGhost(int &x, int &y, bool& gameOver)
 				isCorrectLocation = true;
 			}
 		}
+		// 3 indicates right direction
 		else if (direction == 3)
 		{
 			if (map[y][x + 1] != '#' && map[y][x + 1] != GHOST_CHAR)
@@ -310,6 +332,7 @@ void moveGhost(int &x, int &y, bool& gameOver)
 				isCorrectLocation = true;
 			}
 		}
+		// 4 indicates left direction
 		else if (direction == 4)
 		{
 			if (map[y][x - 1] != '#' && map[y][x - 1] != GHOST_CHAR)
