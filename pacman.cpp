d640a5f49603e4a205e46e0d9b39be634d337435
Suleman Mahmood <sulemanmahmood99@gmail.com>
2019-12-08 14:02:44 +0500

    Update pacman.cpp

--- pacman.cpp
+++ pacman.cpp
@@ -1,107 +1,296 @@
+// Pacman1.0.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
-#include <conio.h>
-#include <windows.h>
+#include <Windows.h>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
-const int width = 24;
-const int height = 24;
-const int spawnx = 12;
-const int spawny = 12;
-const char pacman = '9';
+struct ghostLocation {
+	int x;
+	int y;
+};
 
-char map[width][height];
+const int MAX_FOOD = 10;
+const char FOOD_CHAR = '.';
+const char PACMAN_CHAR = 'C';
+const char GHOST_CHAR = '9';
+const int GENERATE_FOOD_EVERY_SECOND = 10;
+const int SCORE_PER_FOOD = 5;
+const int GHOST_RESPAWN_TIME = 20;
+
+int currentSpawnedFood = 0;
 
 void printMap();
-void initialiseMap();
+void spawnFood();
+ghostLocation spawnGhost();
+void moveGhost(int &x, int &y);
+
+//Default map
+char map[25][25] = {
+	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', '#', '#', '#', '#', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	{'#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	{'#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', '#'},
+	{'#', ' ', '#', ' ', ' ', '#', '#', '#', ' ', '#', ' ', ' ', '#', '#', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', '#'},
+	{'#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' ', '#', ' ', '#'},
+	{'#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#'},
+	{'#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', '#', '#', ' ', '#', '#', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', '#', '#', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' ', '#'},
+	{'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	{'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+};
 
 int main()
 {
-    int x,y;
+	int x, y;
+	int waitTenSecondCounter = 0;
+	int waitGhostRespawnCounter = 0;
+	int Score = 0;
+	int counterGhostSpawned = 0;
+	ghostLocation ghostsLocationsArray[100];
+	ghostLocation newGhost;
 
-    x = spawnx;
-    y = spawny;
+	x = 10; y = 10;
 
-    initialiseMap();
+	// Infinite Loop to run the game
+	while (1)
+	{
+		system("CLS");
 
-    while(1)
-    {
-        Sleep(100);
+		if (GetAsyncKeyState(VK_UP))
+		{
+			if (map[y - 1][x] != '#' && map[y - 1][x] != FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				y--;
+			}
+			else if (map[y - 1][x] == FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				y--;
 
-        printMap();
+				currentSpawnedFood--;
+				Score += SCORE_PER_FOOD;
+			}
+		}
+		else if (GetAsyncKeyState(VK_DOWN))
+		{
+			if (map[y + 1][x] != '#' && map[y + 1][x] != FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				y++;
+			}
+			else if (map[y + 1][x] == FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				y++;
 
-        if(GetAsyncKeyState(VK_UP))
-        {
-            if(map[y][x] != '#')
-            {
-                map[y][x] = ' ';
-                y--;
-            }
-        }
-        else if(GetAsyncKeyState(VK_DOWN))
-        {
-            if(map[y][x] != '#')
-            {
-                map[y][x] = ' ';
-                y++;
-            }
-        }
-        else if(GetAsyncKeyState(VK_LEFT))
-        {
-            if(map[y][x] != '#')
-            {
-                map[y][x] = ' ';
-                x--;
-            }
-        }
-        else if(GetAsyncKeyState(VK_RIGHT))
-        {
-            if(map[y][x] != '#')
-            {
-                map[y][x] = ' ';
-                x++;
-            }
-        }
-        map[y][x] = pacman;
-    }
+				currentSpawnedFood--;
+				Score += SCORE_PER_FOOD;
+			}
+		}
+		else if (GetAsyncKeyState(VK_LEFT))
+		{
+			if (map[y][x-1] != '#' && map[y][x-1] != FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				x--;
+			}
+			else if (map[y][x - 1] == FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				x--;
 
-    return 0;
+				currentSpawnedFood--;
+				Score += SCORE_PER_FOOD;
+			}
+		}
+		else if (GetAsyncKeyState(VK_RIGHT))
+		{
+			if (map[y][x+1] != '#' && map[y][x+1] != FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				x++;
+			}
+			else if (map[y][x + 1] == FOOD_CHAR)
+			{
+				map[y][x] = ' ';
+				x++;
+
+				currentSpawnedFood--;
+				Score += SCORE_PER_FOOD;
+			}
+		}
+
+		map[y][x] = PACMAN_CHAR;
+
+		waitTenSecondCounter++;
+		if (waitTenSecondCounter == GENERATE_FOOD_EVERY_SECOND)
+		{
+			waitTenSecondCounter = 0;
+			spawnFood();
+		}
+
+		waitGhostRespawnCounter++;
+		if (waitGhostRespawnCounter == GHOST_RESPAWN_TIME * 5)
+		{
+			waitGhostRespawnCounter = 0;
+			ghostsLocationsArray[counterGhostSpawned] = spawnGhost();
+			counterGhostSpawned++;
+		}
+
+		cout << "Score: " << Score << endl;
+	
+		for (int i = 0; i < counterGhostSpawned; i++)
+		{
+			moveGhost(ghostsLocationsArray[i].x, ghostsLocationsArray[i].y);
+		}
+		
+
+		printMap();
+
+		//Pauses the game for 100 Millisec
+		Sleep(100);
+	}
+
+	
 }
 
 void printMap()
 {
-    system("CLS");
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j++)
-        {
-            cout << map[i][j];
-        }
-        cout << endl;
-    }
+	for (int i = 0; i < 25; i++)
+	{
+		for (int j = 0; j < 25; j++)
+		{
+			cout << map[i][j];
+		}
+		cout << endl;
+	}
 }
 
-void initialiseMap()
+void spawnFood()
 {
-    for(int i = 0; i < width; i++)
-    {
-        map[0][i] = '#';
-    }
-    for(int i = 1; i < height - 1; i++)
-    {
-        map[i][0] = '#';
+	int x, y;
+	bool isCorrectLocation = false;
 
-        for(int j = 1; j < width - 1; j++)
-        {
-            map[i][j] = ' ';
-        }
+	srand(time(0));
 
-        map[i][width-1] = '#';
-    }
-    for(int i = 0; i < width; i++)
-    {
-        map[height - 1][i] = '#';
-    }
+	if (currentSpawnedFood >= 10)
+		return;
 
-    map[spawny][spawnx] = pacman;
+	while (!isCorrectLocation)
+	{
+		x = rand() % 25;
+		y = rand() % 25;
+
+		if (map[y][x] != '#' && map[y][x] != PACMAN_CHAR)
+		{
+			isCorrectLocation = true;
+			currentSpawnedFood++;
+		}
+			
+	}
+
+	//Spawn Food at x,y
+	map[y][x] = FOOD_CHAR;
+}
+
+ghostLocation spawnGhost()
+{
+	int x, y;
+	bool isCorrectLocation = false;
+	ghostLocation temp;
+
+	srand(time(0));
+
+	while (!isCorrectLocation)
+	{
+		x = rand() % 25;
+		y = rand() % 25;
+
+		if (map[y][x] != '#' && map[y][x] != PACMAN_CHAR)
+		{
+			isCorrectLocation = true;
+		}
+
+	}
+	//spawning our ghost at x,y
+	map[y][x] = GHOST_CHAR;
+
+	temp.x = x;
+	temp.y = y;
+
+	return temp;
+}
+
+void moveGhost(int &x, int &y)
+{
+	bool isCorrectLocation = false;
+
+	srand(time(0));
+
+	while (!isCorrectLocation)
+	{
+		int direction = rand() % 4 + 1;
+
+		if (direction == 1)
+		{
+			if (map[y - 1][x] != '#')
+			{
+				//MOVE UP
+				map[y][x] = ' ';
+				y--;
+				isCorrectLocation = true;
+			}
+		}
+		else if (direction == 2)
+		{
+			if (map[y + 1][x] != '#')
+			{
+				//MOVE DOWN
+				map[y][x] = ' ';
+				y++;
+				isCorrectLocation = true;
+			}
+		}
+		else if (direction == 3)
+		{
+			if (map[y][x + 1] != '#')
+			{
+				//MOVE Right
+				map[y][x] = ' ';
+				x++;
+				isCorrectLocation = true;
+			}
+		}
+		else if (direction == 4)
+		{
+			if (map[y][x - 1] != '#')
+			{
+				//MOVE Left
+				map[y][x] = ' ';
+				x--;
+				isCorrectLocation = true;
+			}
+		}
+	}
+
+	map[y][x] = GHOST_CHAR;
 }
